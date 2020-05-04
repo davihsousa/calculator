62ffdc65a02eeb25f3e61d57a14fdfbeb1f01bab
Davi Henrique de Sousa Pinto <davihenrique.sousa@outlook.com>
2020-05-04 18:31:04 -0300

    Add HTML buttons and JS basic functions

---
+++ script.js
@@ -0,0 +1,16 @@
+const sum = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+const operate = (a, b, operator) => {
+  switch (operator) {
+    case '+':
+      return sum(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+  }
+};
